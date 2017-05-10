ad82d55cad3e56161d34ff9d1ab75f02c6281319
田村崚 <ryo@tamuranoMacBook-Pro.local>
2017-05-10 16:51:10 +0900

    クエリから個別に音楽データを取得。

---
+++ getMusic/MusicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MusicTableViewCell.swift
+//  getMusic
+//
+//  Created by 田村崚 on 2017/05/09.
+//  Copyright © 2017年 ryo.tamura. All rights reserved.
+//
+
+import UIKit
+
+class MusicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- getMusic/ViewController.swift
+++ getMusic/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var arbamLabel: UILabel!
+    @IBOutlet weak var songLabel: UILabel!
+    @IBOutlet weak var artistLabel: UILabel!
+    
+    var player = MPMusicPlayerController()
+    
+    let albumsQuery = MPMediaQuery.songs()
+    
+    let predicate = MPMediaPropertyPredicate(value: 3, forProperty: MPMediaItemPropertyPlayCount, comparisonType: MPMediaPredicateComparison.equalTo)
+    
+    var music : [AnyObject] = []
+    
+   
+    
 
+    
+    @IBAction func handlePlayButton(_ sender: Any) {
+        
+        let query = albumsQuery.collections?[3]
+        // プレイヤーにqueryから作成したキューをセット
+        // (playerはMPMusicPlayerControllerのインスタンスを示す)
+        player.setQueue(with: query!)
+        player.play()
+    }
+    
+    @IBAction func handleStopLabel(_ sender: Any) {
+        player.stop()
+    }
+    
+    
+
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        player = MPMusicPlayerController.applicationMusicPlayer()
+        
+        albumsQuery.addFilterPredicate(predicate)
+        
+        if let collections = albumsQuery.collections {
+            for collection in collections {
+            
+                if let representativeTitle = collection.representativeItem!.title {
+                    print("曲名: \(representativeTitle)  再生回数: \(collection.representativeItem!.playCount)")
+                    music.append(collection.representativeItem!)
+                }
+     
+                
+            }
+        }///if let collections = albumsQuery.collections
+    }///view Did Load
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func updateSongInformationUI(mediaItem: MPMediaItem) {
+        arbamLabel.text = mediaItem.albumTitle ?? "不明なアルバム"
+        songLabel.text = mediaItem.title ?? "不明な曲"
+        artistLabel.text = mediaItem.artist ?? "不明なアーティスト"
+        
+        if let artwork = mediaItem.artwork {
+            let image = artwork.image(at: imageView.bounds.size)
+            imageView.image = image
+        }else{
+            imageView.image = nil
+            imageView.backgroundColor = UIColor.black
+        }
+    }
+    
+   
 
 }
 
